306b495c272fe098f658e6bfaa205494b9fef09e
kylemocode <kylemo860617@gmail.com>
2021-12-06 12:05:42 +0800

    feature: html_parser

--- src/dom.rs
+++ src/dom.rs
@@ -1,74 +1,101 @@
 use std::collections::{HashMap, HashSet};
+use std::fmt;
 
-#[derive(Debug)]
-struct Node {
-  children: Vec<Node>,
-  node_type: NodeType,
+#[derive(PartialEq, Eq)]
+pub struct Node {
+    pub children: Vec<Node>,
+    pub node_type: NodeType,
 }
 
-#[derive(Debug)]
-enum NodeType {
-  Text(String),
-  Element(ElementData),
-  Comment(String),
+#[derive(PartialEq, Eq, Clone)]
+pub enum NodeType {
+    Text(String),
+    Element(ElementData),
+    Comment(String),
 }
 
-#[derive(Debug)]
-struct ElementData {
-  tag_name: String,
-  attributes: AttrMap,
+#[derive(PartialEq, Eq, Clone)]
+pub struct ElementData {
+    pub tag_name: String,
+    attributes: AttrMap,
 }
 
 impl ElementData {
-  fn new(tag_name: String, attributes: AttrMap) -> ElementData {
-    ElementData {
-      tag_name,
-      attributes,
+    pub fn new(tag_name: String, attributes: AttrMap) -> ElementData {
+        ElementData {
+            tag_name,
+            attributes,
+        }
     }
-  }
 
-  fn get_id(&self) -> Option<&String> {
-    self.attributes.get("id")
-  }
-
-  fn get_classes(&self) -> HashSet<&str> {
-    match self.attributes.get("class") {
-      Some(s) => s.split(" ").collect(),
-      None => HashSet::new(),
+    pub fn get_id(&self) -> Option<&String> {
+        self.attributes.get("id")
     }
-  }
+
+    pub fn get_classes(&self) -> HashSet<&str> {
+        match self.attributes.get("class") {
+            Some(s) => s.split(' ').collect(),
+            None => HashSet::new(),
+        }
+    }
 }
 
-type AttrMap = HashMap<String, String>;
+pub type AttrMap = HashMap<String, String>;
 
 impl Node {
-  fn new(node_type: NodeType, children: Vec<Node>) -> Node {
-    Node {
-      node_type,
-      children,
+    pub fn new(node_type: NodeType, children: Vec<Node>) -> Node {
+        Node {
+            node_type,
+            children,
+        }
     }
-  }
 }
 
-fn pretty_print(n: &Node, indent_size: usize) {
-  let indent = (0..indent_size).map(|_| " ").collect::<String>();
-
-  match n.node_type {
-    NodeType::Element(ref e) => println!("{}{:?}", indent, e),
-    NodeType::Text(ref t) => println!("{}{}", indent, t),
-    NodeType::Comment(ref c) => println!("{}<-!--{}-->", indent, c),
-  }
-
-  for child in n.children.iter() {
-    pretty_print(&child, indent_size + 2);
-  }
-
-  match n.node_type {
-    NodeType::Element(ref e) => println!("{}<{}/>", indent, e.tag_name),
-    _ => {}
-  }
+impl fmt::Debug for Node {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:?}", self.node_type)
+    }
 }
 
+impl fmt::Debug for NodeType {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match *self {
+            NodeType::Text(ref t) | NodeType::Comment(ref t) => write!(f, "{}", t),
+            NodeType::Element(ref e) => write!(f, "{:?}", e),
+        }
+    }
+}
+
+impl fmt::Debug for ElementData {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let mut attributes_string = String::new();
+
+        for (attr, value) in self.attributes.iter() {
+            attributes_string.push_str(&format!(" {}=\"{}\"", attr, value));
+        }
+        write!(f, "<{},{}>", self.tag_name, attributes_string)
+    }
+}
+
+
+pub fn pretty_print(n: &Node, indent_size: usize) {
+    let indent = (0..indent_size).map(|_| " ").collect::<String>();
+
+    match n.node_type {
+        NodeType::Element(ref e) => println!("{}{:?}", indent, e),
+        NodeType::Text(ref t) => println!("{}{}", indent, t),
+        NodeType::Comment(ref c) => println!("{}<!--{}-->", indent, c),
+    }
+
+    for child in n.children.iter() {
+        pretty_print(&child, indent_size + 2);
+    }
+
+    match n.node_type {
+        NodeType::Element(ref e) => println!("{}<{}/>", indent, e.tag_name),
+        _ => {}
+    }
+}
 // pub fn text(data: String) -> Node {
 //   Node { children: vec![], node_type: NodeType::Text(data) }
 // }
